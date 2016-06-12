099167170e405039b986b6a2493e73c31f10efe9
Alex Pacheco <alex.pacheco.s@gmail.com>
2016-06-11 23:42:08 -0500

    optimizando P3 con SLICE()

--- p3/js/p3.js
+++ p3/js/p3.js
@@ -1,16 +1,32 @@
 // Se pide una función que recibe un arreglo como parámetro y devuelva una copia del arreglo.
 // Probar con un ejemplo sencillo que funcione.
 
+//
+// var copia_arreglo = function(array) {
+//   if (!(array instanceof Array)) {
+//     console.log("No es un array")
+//     return false
+//   }else {
+//     var new_array = []
+//     array.forEach(function(e) {
+//       new_array.push(e)
+//     })
+//     return new_array
+//   }
+// }
 
 var copia_arreglo = function(array) {
   if (!(array instanceof Array)) {
     console.log("No es un array")
     return false
   }else {
-    var new_array = []
-    array.forEach(function(e) {
-      new_array.push(e)
-    })
-    return new_array
+    return array.slice(0);
   }
 }
+
+
+var arr1 = []
+var arr2 = copia_arreglo(arr1)
+arr1.push(23)
+console.log("arr1= "+arr1)
+console.log("arr2= "+arr2)
