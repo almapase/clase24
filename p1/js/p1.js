4b440eb9de4d2713aefb847d222c4e4d64f709ff
Alex Pacheco <alex.pacheco.s@gmail.com>
2016-06-13 02:15:20 -0500

    optimizando P1  con FILTER y REDUCE

--- p1/js/p1.js
+++ p1/js/p1.js
@@ -4,15 +4,19 @@ var suma_arreglo = function(array) {
     console.log("No es un array")
     return false
   }else {
-    array.forEach(function(e) {
-      if (!isNaN(e)) {
-        suma += e
-      }
-    })
-    return suma
+    return array.filter(function(e){return !isNaN(e)}).reduce(function(acum, e){return acum + e})
+    // array.forEach(function(e) {
+    //   if (!isNaN(e)) {
+    //     suma += e
+    //   }
+    // })
+    // return suma
   }
 }
 
 var numeros = [1,2,3,4,5,6]
+var suma = suma_arreglo(numeros)
+console.log(suma)
+var numeros = [1,2,3,"f",4,5,6]
 var suma = suma_arreglo(numeros)
 console.log(suma)
